bae856c733e5eace489f064cdc7ab4e2e7ecb68f
NathanaelDEV93 <natanaeldev93@gmail.com>
2024-03-05 23:32:03 -0300

    exemplo de import de outro arquivo

---
+++ import_outro_arquivo.py
@@ -0,0 +1,11 @@
+# arquivo1.py
+# minha_variavel = "Olá, mundo!"
+
+# if __name__ == "__main__":
+#    print("Este código só será executado se este arquivo for executado diretamente.")
+
+
+# arquivo2.py
+# from arquivo1 import minha_variavel
+
+# print(minha_variavel)  # Isso imprimirá: Olá, mundo!
